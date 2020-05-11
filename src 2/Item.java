fd0f5fc43f72dea0620f1216a112f0d9f281841e
lautaro terzano <lautaroterzano>
2020-05-11 11:34:47 -0300

    Cree las clases Item, Inventario y Sitio, no terminadas

---
+++ src 2/Item.java
@@ -0,0 +1,15 @@
+
+public class Item {
+	
+	String nombre;
+	float peso;
+	char gender;
+	char number;
+	String[] acciones;
+	String[] afecta;
+	
+	
+	public float getPeso() {
+		return this.peso;
+	}
+}
