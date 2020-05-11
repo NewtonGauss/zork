0531a325829534f75a07d73e425f60d89e9d430d
lautaro terzano <lautaroterzano>
2020-05-11 12:15:43 -0300

    Cree las clases Inventario, Sitio, Item, Character. No estan terminadas

--- src 2/Inventario.java
+++ src 2/Inventario.java
@@ -3,20 +3,17 @@ public class Inventario {
 	
 	float pesoMax;
 	float pesoActual;
-	java.util.List<Item> items = new java.util.ArrayList<Item>();
 	
 	
 	public boolean addItem(Item nuevoItem) {
 		boolean bool = false;
 		if(nuevoItem.getPeso() + this.pesoActual <= this.pesoMax) {
 			this.pesoActual += nuevoItem.getPeso();
-			items.add(nuevoItem);
 			bool = true;
 		}
 		return bool;
 	}
 	
 	public void removeItem(Item sacar) {
-		items.remove(sacar);
 	}
 }
--- src 2/Sitio.java
+++ src 2/Sitio.java
@@ -1,4 +1,8 @@
 
 public class Sitio {
-
+	
+	String nombre;
+	char gender;
+	char number;
+	Item[] items;
 }
