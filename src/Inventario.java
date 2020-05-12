04d1e61ae6356173eeb0f200db4ec97e5b919b07
NewtonGauss <germanmerkel9@gmail.com>
2020-05-12 16:46:09 -0300

    get items devuelve iterador

--- src/Inventario.java
+++ src/Inventario.java
@@ -1,43 +1,37 @@
 import java.util.Hashtable;
+import java.util.Iterator;
 
 public class Inventario {
-	
-	private Double pesoMax=200.0;
-	private Double pesoActual=0.0;
-	Hashtable<String, Item> inventario = new Hashtable<>();
-	
-	
-	public boolean addItem(Item nuevoItem) {
-		boolean bool = false;
-		if(nuevoItem.getPeso() + this.pesoActual <= this.pesoMax) {
-			this.pesoActual += nuevoItem.getPeso();
-			inventario.put(nuevoItem.getNombre(), nuevoItem);
-			bool = true;
-		}
-		return bool;
+
+    private Double pesoMax = 200.0;
+    private Double pesoActual = 0.0;
+    Hashtable<String, Item> inventario = new Hashtable<>();
+
+    public boolean addItem(Item nuevoItem) {
+	boolean bool = false;
+	if (nuevoItem.getPeso() + this.pesoActual <= this.pesoMax) {
+	    this.pesoActual += nuevoItem.getPeso();
+	    inventario.put(nuevoItem.getNombre(), nuevoItem);
+	    bool = true;
 	}
-	
-	
-	public Item removeItem(Item sacar) {
-		inventario.remove(sacar.getNombre());
-		pesoActual -= sacar.getPeso();
-		return sacar;
-	}
-	
-	public Item getItem(String nombre) {
-		return inventario.get(nombre);
-	}
-	
-	
-	/* ES DE  PRUEBA, SE PODRIA IMPLEMENTAR PARA QUE DEVUELVA LA HASHTABLE */
-	public void getItems() {
-		inventario.forEach((k,v)->{
-			System.out.println(k.toString() + " " + v.getPeso());
-		});
-	}
-	
-	
-	public Double getPesoActual() {
-		return this.pesoActual;
-	}
+	return bool;
+    }
+
+    public Item removeItem(Item sacar) {
+	inventario.remove(sacar.getNombre());
+	pesoActual -= sacar.getPeso();
+	return sacar;
+    }
+
+    public Item getItem(String nombre) {
+	return inventario.get(nombre);
+    }
+
+    public Iterator<Item> getItems() {
+	return inventario.values().iterator();
+    }
+
+    public Double getPesoActual() {
+	return this.pesoActual;
+    }
 }
