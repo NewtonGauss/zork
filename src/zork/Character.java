c897452f75d91578fbde680b44491caf11ff608f
NewtonGauss <germanmerkel9@gmail.com>
2020-05-14 11:09:19 -0300

    Abstract character completo

--- src/zork/Character.java
+++ src/zork/Character.java
@@ -1,39 +1,46 @@
 package zork;
 
+import java.util.Iterator;
+
 public abstract class Character {
-	
-	private Inventario inventario;
-	private String nombre;
-	private float salud;
-//	float saludMaxima
-	private char genero;
-	
-	public abstract boolean addItem(Item nuevoItem);
-	
-	public abstract void removeItem(Item sacar);
-	
-	public void sumarSalud(float healthup) {
-		if(isDead() == false) {
-			this.salud += healthup;
-//			if(this.salud > this.saludMaxima){
-//				this.salud = saludMaxima;
-//			}
-		}
+
+    private Inventario inventario;
+    private float salud;
+
+    public boolean addItem(Item nuevoItem) {
+	return inventario.addItem(nuevoItem);
+    }
+
+    public boolean removeItem(String nombreItem) {
+	boolean bool = false;
+	Item i = inventario.getItem(nombreItem);
+	if ( i != null) {
+	    inventario.removeItem(i);
+	    bool = true;
 	}
-	
-	public void restarSalud(float healthdown) {
-		if(this.salud - healthdown <= 0 && isDead() == false) {
-			this.salud = 0;
-		}else {
-			this.salud -= healthdown;
-		}
+	return bool;
+    }
+    
+    public Iterator<Item> getItems() {
+	return inventario.getItems();
+    }
+
+    public void sumarSalud(float healthup) {
+	if (!isDead()) {
+	    this.salud += healthup;
 	}
-	
-	public boolean isDead() {
-		return this.salud == 0 ? true : false;
-	};
-	
-	public char getGender() {
-		return this.genero;
+    }
+
+    public void restarSalud(float healthdown) {
+	if (this.salud - healthdown <= 0 && isDead() == false) {
+	    this.salud = 0;
+	} else {
+	    this.salud -= healthdown;
 	}
+    }
+
+    public boolean isDead() {
+	return this.salud == 0 ? true : false;
+    };
+
 }
