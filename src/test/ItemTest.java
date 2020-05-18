77086de0da69c4d19c030eb2c8041e442f43530d
NewtonGauss <germanmerkel9@gmail.com>
2020-05-18 12:44:56 -0300

    Test item completo

--- src/test/ItemTest.java
+++ src/test/ItemTest.java
@@ -9,11 +9,22 @@ import com.google.gson.JsonParser;
 import zork.Item;
 
 class ItemTest {
-    private String json = " {\n" + "      \"name\": \"barreta\",\n" + "      \"gender\": \"female\",\n"
-	    + "      \"number\": \"singular\",\n" + "\t\t\t\"weight\": \"10\",\n" + "\t\t\t\"type\": \"weapon\",\n" 
-    	+"      \"actions\": [\n" + "        \"usar\"\n" + "      ],\n"
-	    + "      \"effects_over\": [\n" + "        \"npcs\",\n" + "        \"self\",\n" + "        \"item\"\n"
-	    + "      ]\n" + "    }";
+    private String json = "{\n" + 
+    	"      \"name\": \"barreta\",\n" + 
+    	"      \"gender\": \"female\",\n" + 
+    	"      \"number\": \"singular\",\n" + 
+    	"			\"points\": \"100\",\n" + 
+    	"			\"weight\": \"10\",\n" + 
+    	"			\"type\": \"weapon\",\n" + 
+    	"      \"actions\": [\n" + 
+    	"        \"usar\"\n" + 
+    	"      ],\n" + 
+    	"      \"effects_over\": [\n" + 
+    	"        \"npcs\",\n" + 
+    	"        \"self\",\n" + 
+    	"        \"item\"\n" + 
+    	"      ]\n" + 
+    	"    }";
 
     @Test
     void test01() {
@@ -31,6 +42,81 @@ class ItemTest {
     void test03() {
 	Item i = new Item(JsonParser.parseString(json));
 	assertEquals(false, i.esUsoValido("atacar"));
-    }   
+    }
+    
+    @Test
+    void testObjetoMale() {
+	String json = "{\n" + 
+	    	"      \"name\": \"palo\",\n" + 
+	    	"      \"gender\": \"male\",\n" + 
+	    	"      \"number\": \"singular\",\n" + 
+	    	"			\"points\": \"100\",\n" + 
+	    	"			\"weight\": \"10\",\n" + 
+	    	"			\"type\": \"weapon\",\n" + 
+	    	"      \"actions\": [\n" + 
+	    	"        \"usar\"\n" + 
+	    	"      ],\n" + 
+	    	"      \"effects_over\": [\n" + 
+	    	"        \"npcs\",\n" + 
+	    	"        \"self\",\n" + 
+	    	"        \"item\"\n" + 
+	    	"      ]\n" + 
+	    	"    }";
+	Item i = new Item(JsonParser.parseString(json));
+	assertEquals("el palo", i.toString());
+    }
+    
+    @Test
+    void testObjetoMalePlural() {
+	String json = "{\n" + 
+	    	"      \"name\": \"palos\",\n" + 
+	    	"      \"gender\": \"male\",\n" + 
+	    	"      \"number\": \"plural\",\n" + 
+	    	"			\"points\": \"100\",\n" + 
+	    	"			\"weight\": \"10\",\n" + 
+	    	"			\"type\": \"weapon\",\n" + 
+	    	"      \"actions\": [\n" + 
+	    	"        \"usar\"\n" + 
+	    	"      ],\n" + 
+	    	"      \"effects_over\": [\n" + 
+	    	"        \"npcs\",\n" + 
+	    	"        \"self\",\n" + 
+	    	"        \"item\"\n" + 
+	    	"      ]\n" + 
+	    	"    }";
+	Item i = new Item(JsonParser.parseString(json));
+	assertEquals("los palos", i.toString());
+    }
+    
+    @Test
+    void testObjetoFemalePlural() {
+	String json = "{\n" + 
+	    	"      \"name\": \"barretas\",\n" + 
+	    	"      \"gender\": \"female\",\n" + 
+	    	"      \"number\": \"plural\",\n" + 
+	    	"			\"points\": \"100\",\n" + 
+	    	"			\"weight\": \"10\",\n" + 
+	    	"			\"type\": \"weapon\",\n" + 
+	    	"      \"actions\": [\n" + 
+	    	"        \"usar\"\n" + 
+	    	"      ],\n" + 
+	    	"      \"effects_over\": [\n" + 
+	    	"        \"npcs\",\n" + 
+	    	"        \"self\",\n" + 
+	    	"        \"item\"\n" + 
+	    	"      ]\n" + 
+	    	"    }";
+	Item i = new Item(JsonParser.parseString(json));
+	assertEquals("las barretas", i.toString());
+    }
+    
+    @Test
+    void testGetters() {
+	Item i = new Item(JsonParser.parseString(json));
+	assertEquals("barreta", i.getNombre());
+	assertEquals(10d, i.getPeso());
+	assertEquals("weapon", i.getTipo());
+	assertEquals(100, i.getPoints());
+    }
 
 }
--- src/zork/Item.java
+++ src/zork/Item.java
@@ -1,65 +1,72 @@
 package zork;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-public class Item
-{
+public class Item {
 
-	private String nombre;
-	private Double peso;
-	private char gender;
-	private char number;
-	private int points;
-	private List<String> accionesValidas = new ArrayList<String>();
-	private List<String> afecta = new ArrayList<String>();
-	private String tipo;
+    private String nombre;
+    private Double peso;
+    private char gender;
+    private char number;
+    private int points;
+    private List<String> accionesValidas = new ArrayList<String>();
+    private List<String> afecta = new ArrayList<String>();
+    private String tipo;
 
-	/*
-	 * Crea el objeto desde un elemento JSON.
-	 */
-	public Item(JsonElement json) {
-		JsonObject jobject = json.getAsJsonObject();
-		nombre = jobject.get("name").getAsString();
-		peso = jobject.get("weight").getAsDouble();
-		points = jobject.get("points").getAsInt();
-		tipo = jobject.get("type").getAsString();
-		gender = jobject.get("gender").getAsString().equals("male") ? 'm' : 'f';
-		number = jobject.get("number").getAsString().equals("singular") ? 's' : 'p';
-		for (JsonElement accion : jobject.getAsJsonArray("actions"))
-			accionesValidas.add(accion.getAsString());
-		for (JsonElement effectOn : jobject.getAsJsonArray("effects_over"))
-			afecta.add(effectOn.getAsString());
-	}
+    /*
+     * Crea el objeto desde un elemento JSON.
+     */
+    public Item(JsonElement json) {
+	JsonObject jobject = json.getAsJsonObject();
+	nombre = jobject.get("name").getAsString();
+	peso = jobject.get("weight").getAsDouble();
+	points = jobject.get("points").getAsInt();
+	tipo = jobject.get("type").getAsString();
+	gender = jobject.get("gender").getAsString().equals("male") ? 'm' : 'f';
+	number = jobject.get("number").getAsString().equals("singular") ? 's' : 'p';
+	for (JsonElement accion : jobject.getAsJsonArray("actions"))
+	    accionesValidas.add(accion.getAsString());
+	for (JsonElement effectOn : jobject.getAsJsonArray("effects_over"))
+	    afecta.add(effectOn.getAsString());
+    }
 
-	public Double getPeso() { return this.peso; }
+    public Double getPeso() {
+	return this.peso;
+    }
 
-	public String getNombre() { return this.nombre; }
-	
-	public String getTipo() { return this.tipo; }
+    public String getNombre() {
+	return this.nombre;
+    }
 
-	@Override
-	public String toString() {
-		String fraseItem = "";
-		if (number == 's')
-			fraseItem += gender == 'm' ? "el " : "la ";
-		else
-			fraseItem += gender == 'm' ? "los " : "las ";
-		return fraseItem + nombre;
-	}
+    public String getTipo() {
+	return this.tipo;
+    }
 
-	public boolean esUsoValido(String uso) {
+    public int getPoints() {
+	return points;
+    }
 
-		return accionesValidas.contains(uso);
-	}
+    public boolean esUsoValido(String uso) {
 
-	public boolean esObjetivoValido(String objetivo) {
-		return afecta.contains(objetivo);
-	}
+	return accionesValidas.contains(uso);
+    }
 
-	public int getPoints() {
-	    return points;
-	}
+    public boolean esObjetivoValido(String objetivo) {
+	return afecta.contains(objetivo);
+    }
+
+    @Override
+    public String toString() {
+	String fraseItem = "";
+	if (number == 's')
+	    fraseItem += gender == 'm' ? "el " : "la ";
+	else
+	    fraseItem += gender == 'm' ? "los " : "las ";
+	return fraseItem + nombre;
+    }
+
 }
