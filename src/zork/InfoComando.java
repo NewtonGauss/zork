4825b7d78d453f7152b44c4eee3384ea18051f95
lautaroterzano <lautaroterzano@gmail.com>
2020-05-19 16:49:00 -0300

    comando poner

--- src/zork/InfoComando.java
+++ src/zork/InfoComando.java
@@ -4,14 +4,14 @@ public class InfoComando implements Comando{
 	
 	/*
 	 * PARA QUE FUNCIONE EN EL PARAMETRO RESTODELCOMANDO SE TIENE QUE RECIBIR 
-	 * "nombreItem:nombreNPC"
+	 * "info del Json"
 	 * 
 	 * */
 
 	@Override
 	public String ejecutar(Jugador jugador, String restoDelComando) {
 		
-		return jugador.getHabitacionActual().getDescription();
+		return restoDelComando;
 	}
 
 }
