21f90c18ec40c00c07767afa2caf24d1d3fc4c47
NewtonGauss <germanmerkel9@gmail.com>
2020-05-20 21:58:04 -0300

    Refactorizado, me da cosa tanto if..

--- src/zork/UsarItemComando.java
+++ src/zork/UsarItemComando.java
@@ -2,6 +2,8 @@ package zork;
 
 public class UsarItemComando implements Comando {
 
+    private static final String NO_HA_SERVIDO = "Eso no ha servido de nada.";
+
     /*
      * Manera de usar: ejecutar(jugador,nombreItem + ":" + nombreNPC) el nombre del
      * npc puede ser opcional. si no se psa por parametro el nombre del npc, el item
@@ -9,39 +11,64 @@ public class UsarItemComando implements Comando {
      */
     @Override
     public String ejecutar(Jugador jugador, String restoDelComando) {
-	
 	String[] parseado = restoDelComando.split(":");
 	String itemNombre = parseado[0];
 	String retorno = "";
 	Item item = jugador.getItem(itemNombre);
-	
+
 	if (item != null && !item.esUsoValido("usar"))
-	    return "Eso no ha servido de nada.";
-	
-	if (parseado.length == 2 && item != null) {
-	    String npcNombre = parseado[1];
-	    NPC npc = jugador.habitacionActual.getNPC(npcNombre);
-	    if (item.esObjetivoValido("npcs") && npc != null && isItemAplicable(item.getTipo())) {
-		retorno = "Se utilizo " + item.toString() + " sobre " + npc.toString() + ".\n";
-		String trigger = npc.ejecutarTrigger(Trigger.ITEM, item.getNombre());
-		if (trigger != null)
-		    retorno += trigger;
-		else
-		    npc.aplicarItem(item.getTipo());
-		jugador.removeItem(item.getNombre());
-	    } else if (npc != null)
-		retorno = "Eso no ha servido de nada.";
-	    else
-		retorno = npcNombre + " no se encuentra en " + jugador.habitacionActual.toString() + ".";
-	} else if(item != null && item.esObjetivoValido("self") && isItemAplicable(item.getTipo())) {
-		jugador.aplicarItem(item.getTipo());
-		jugador.removeItem(item.getNombre());
-		retorno = "Se utilizo " + item.toString() + " sobre ti." ;
-	} else if (item != null)
-	    retorno = "Eso no ha servido de nada.";
+	    retorno = NO_HA_SERVIDO;
+
+	else if (parseado.length == 2 && item != null)
+	    retorno = usarSobreNPC(jugador, parseado[1], item);
+
+	else if (item != null && item.esObjetivoValido("self")
+		&& isItemAplicable(item.getTipo()))
+	    retorno = usarSobreJugador(jugador, item);
+
+	else if (item != null)
+	    retorno = NO_HA_SERVIDO;
 	else
 	    retorno = "No cuenta con " + itemNombre + " en el inventario.";
 
+	return retorno;
+    }
+
+    private String usarSobreNPC(Jugador jugador, String npcNombre, Item item) {
+	String retorno;
+	NPC npc = jugador.habitacionActual.getNPC(npcNombre);
+	if (npc != null)
+	    retorno = aplicarSobreNpc(jugador, item, npc);
+	else
+	    retorno = npcNombre + " no se encuentra en "
+		    + jugador.habitacionActual.toString() + ".";
+	return retorno;
+    }
+
+    private String aplicarSobreNpc(Jugador jugador, Item item, NPC npc) {
+	String retorno;
+	if (item.esObjetivoValido("npcs")) {
+	    retorno = "Se utilizo " + item.toString() + " sobre " + npc.toString()
+		    + ".\n";
+
+	    String trigger = npc.ejecutarTrigger(Trigger.ITEM, item.getNombre());
+	    if (trigger != null)
+		retorno += trigger;
+	    else if (isItemAplicable(item.getTipo())) {
+		npc.aplicarItem(item.getTipo());
+		jugador.removeItem(item.getNombre());
+	    } else
+		retorno = NO_HA_SERVIDO;
+	} else
+	    retorno = NO_HA_SERVIDO;
+	return retorno;
+    }
+
+    private String usarSobreJugador(Jugador jugador, Item item) {
+	String retorno;
+	jugador.aplicarItem(item.getTipo());
+	jugador.removeItem(item.getNombre());
+	retorno = "Se utilizo " + item.toString() + " sobre ti.";
 	return retorno;
     }
 
